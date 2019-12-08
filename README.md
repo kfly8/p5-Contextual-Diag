31efbe876ca8e24237409d5430a2254062137fbe
kfly8 <kfly@cpan.org>
2019-12-08 23:01:45 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Contextual::Diag - It's new $module
+
+# SYNOPSIS
+
+    use Contextual::Diag;
+
+# DESCRIPTION
+
+Contextual::Diag is ...
+
+# LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kfly8 <kfly@cpan.org>
---
+++ lib/Contextual/Diag.pm
@@ -0,0 +1,39 @@
+package Contextual::Diag;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Contextual::Diag - It's new $module
+
+=head1 SYNOPSIS
+
+    use Contextual::Diag;
+
+=head1 DESCRIPTION
+
+Contextual::Diag is ...
+
+=head1 LICENSE
+
+Copyright (C) kfly8.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kfly8 E<lt>kfly@cpan.orgE<gt>
+
+=cut
+
